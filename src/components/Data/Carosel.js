4560538774519b4b1eb96db57060c29d50a0523e
SamIfesi <Ekesamrose@gmail.com>
2026-03-19 21:21:46 +0100

    Adds image data array for carousel feature
    Introduces a structured list of image sources and alt text to support a carousel component, enabling dynamic rendering of carousel images. Facilitates scalable and maintainable management of carousel assets.

---
+++ src/components/Data/Carosel.js
@@ -0,0 +1,10 @@
+export const Carosel = [
+  {img: '/assets/img/carosel/carosel1.jpg', alt: 'Carosel Image 1'},
+  {img: '/assets/img/carosel/carosel2.jpg', alt: 'Carosel Image 2'},
+  {img: '/assets/img/carosel/carosel3.jpg', alt: 'Carosel Image 3'},
+  {img: '/assets/img/carosel/carosel4.jpg', alt: 'Carosel Image 4'},
+  {img: '/assets/img/carosel/carosel5.jpg', alt: 'Carosel Image 5'},
+  {img: '/assets/img/carosel/carosel6.jpg', alt: 'Carosel Image 6'},
+  {img: '/assets/img/carosel/carosel7.jpg', alt: 'Carosel Image 7'},
+
+]

d8eb8639b1acf8a86b6bfdf45e69c2a931656341
SamIfesi <Ekesamrose@gmail.com>
2026-03-20 11:55:15 +0100

    Adds new carousel images and updates image data
    Introduces additional images for the carousel feature and creates a data structure to manage carousel image sources and alt text. Enhances the visual variety and flexibility of image display in the application.

---
+++ src/components/Data/Carousel.js
@@ -0,0 +1,16 @@
+export const CarouselImg = [
+  {img: '/assets/img/carousel/carousel.jpg', alt: 'Carousel Image'},
+  {img: '/assets/img/carousel/carousel1.jpg', alt: 'Carousel Image 1'},
+  {img: '/assets/img/carousel/carousel2.jpg', alt: 'Carousel Image 2'},
+  {img: '/assets/img/carousel/carousel3.jpg', alt: 'Carousel Image 3'},
+  {img: '/assets/img/carousel/carousel4.jpg', alt: 'Carousel Image 4'},
+  {img: '/assets/img/carousel/carousel5.jpg', alt: 'Carousel Image 5'},
+  {img: '/assets/img/carousel/carousel6.jpg', alt: 'Carousel Image 6'},
+  {img: '/assets/img/carousel/carousel7.jpg', alt: 'Carousel Image 7'},
+  {img: '/assets/img/carousel/carousel8.jpg', alt: 'Carousel Image 8'},
+  {img: '/assets/img/carousel/carousel9.jpg', alt: 'Carousel Image 9'},
+  {img: '/assets/img/carousel/carousel10.jpg', alt: 'Carousel Image 10'},
+  {img: '/assets/img/carousel/carousel11.jpg', alt: 'Carousel Image 11'},
+  {img: '/assets/img/carousel/carousel12.jpg', alt: 'Carousel Image 12'},
+
+]
